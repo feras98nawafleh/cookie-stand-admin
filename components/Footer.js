310b60f4239d70058f6eb52e6470b7a0dd9477e0
Feras Nawafleh <85874246+feras98nawafleh@users.noreply.github.com>
2021-12-20 20:05:32 +0200

    Update Footer.js

--- components/Footer.js
+++ components/Footer.js
@@ -2,9 +2,9 @@ const Footer = () => {
   return (
     <>
       <footer className="absolute bottom-0 w-full	flex justify-between bg-green-500 py-4 items-center">
-        <a href="career" className="m-1">
+        <p href="career" className="m-1">
           &#169;2021
-        </a>
+        </p>
       </footer>
     </>
   );
